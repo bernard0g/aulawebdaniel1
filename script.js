5ba8d11b282e22361207336b30736eda380e91a9
bernard0g <bernardelax@gmail.com>
2025-03-21 20:55:30 -0300

    aula js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-console.log("oi console");
+/*console.log("oi console");
+console.info("infraero");
+console.warn("fica esperto");
+console.error("quebrou tudo");
+
+console.time("Timer")
+ //codigo
+console.timeEnd("Timer")
+
 alert("trojan velho");
-prompt("manda 50 no pix?")
+prompt("manda 50 no pix?"); */
+
+let loginDoUsuario = {
+    login: "simio",
+    senha: "biricutico",
+}
+
+let loginDosUsuarios = [
+    {id: 1, login: "douglas", senha: "simio"},
+    {id: 2, login: "marcela", senha: "coisas2"},
+]
+
+console.log(loginDoUsuario);
+console.log(loginDosUsuarios);
